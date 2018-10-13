f6f1a50abe7f6ca0ca21ade9c1850ceb58f9961e
startung <717612+startung@users.noreply.github.com>
2018-10-13 17:33:08 +0200

    v2 now uses a manually weighted neural network

--- v2/tttcheck.c
+++ v2/tttcheck.c
@@ -23,12 +23,68 @@
 #define TRUE 1
 #define FALSE 0
 
-int main(int argc, char *argv[]) {
-	_Bool winning_move = FALSE;
-	char grid[10];
-	scanf("%s", grid);
+#define NUMBER_INPUT 9
+#define NUMBER_HIDDEN 8
 
-out:
-	(winning_move) ? printf("YES\n") : printf("NO\n");
+void create_input_nodes(int nodes[]);
+int abs(int x);
+int activation(int weights[], int inputs[]);
+_Bool result(int nodes[8][9], int inputs[]);
+
+int main(int argc, char *argv[]) {
+	int input_nodes[NUMBER_INPUT] = {0};
+
+	// add tic-tac-toe specific weights
+	int hidden_nodes[8][9] = {{1,1,1,0,0,0,0,0,0},
+				  {0,0,0,1,1,1,0,0,0},
+				  {0,0,0,0,0,0,1,1,1},
+				  {1,0,0,1,0,0,1,0,0},
+				  {0,1,0,0,1,0,0,1,0},
+				  {0,0,1,0,0,1,0,0,1},
+				  {1,0,0,0,1,0,0,0,1},
+				  {0,0,1,0,1,0,1,0,0}};
+	
+	create_input_nodes(input_nodes); // from input
+
+	// calculate result
+	(result(hidden_nodes, input_nodes)) ? printf("YES\n") : printf("NO\n");
 	return 0;
+}
+
+void create_input_nodes(int nodes[]) { // create input nodes
+	char ch;
+	for (int i = 0; i < NUMBER_INPUT; i++) {
+		ch = getchar();
+		switch (ch) {
+			case 'X':
+				nodes[i] = 1;
+				break;
+			case 'O':
+				nodes[i] = -1;
+				break;
+		}
+	}
+}
+
+int abs(int x){
+	return (x < 0 ? -1*x : x);
+}
+
+int activation(int weights[], int inputs[]){
+	int activation = 0;
+	for(int i = 0; i < NUMBER_INPUT; i++) {
+		activation += weights[i] * inputs[i];
+	}
+	return (abs(activation));
+}
+
+_Bool result(int nodes[8][9], int inputs[]){
+	_Bool result = FALSE;
+	for(int i = 0; i < NUMBER_HIDDEN; i++) {
+		if(activation(nodes[i], inputs) > 1) {
+			result = TRUE;
+			break;
+		}
+	}
+	return result;
 }
